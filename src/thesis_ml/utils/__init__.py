68b61e118e60918ebf0ad5655fd3dadfe42c7923
nielsterLinden <nielsterlinden@gmail.com>
2025-10-21 17:52:54 +0200

    commit at end of day, forward pass notebook now works with refactored models. All now split up

--- src/thesis_ml/utils/__init__.py
+++ src/thesis_ml/utils/__init__.py
@@ -1,2 +1,11 @@
+from .phase1_infer import get_example_batch, load_model_from_run, reconstruct_event  # noqa: F401
 from .plotting import plot_loss_curve  # noqa: F401
 from .seed import set_all_seeds  # noqa: F401
+
+__all__ = [
+    "plot_loss_curve",
+    "set_all_seeds",
+    "load_model_from_run",
+    "get_example_batch",
+    "reconstruct_event",
+]
