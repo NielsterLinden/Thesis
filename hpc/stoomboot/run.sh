8f8b6d5e222fdf7d8472aa3d4efc577216486aa7
nielsterLinden <nielsterlinden@gmail.com>
2025-10-29 12:45:12 +0100

    stoomboot push

--- hpc/stoomboot/run.sh
+++ hpc/stoomboot/run.sh
@@ -8,11 +8,11 @@ echo "Working directory: $(pwd)"
 echo "Job ID: ${CONDOR_JOB_ID:-N/A}"
 
 # Navigate to code directory
-cd /project/atlas/nterlind/Thesis-Code
+cd /project/atlas/users/nterlind/Thesis-Code
 
 # Activate conda environment
 eval "$(conda shell.bash hook)"
-conda activate /data/atlas/nterlind/venvs/thesis-ml
+conda activate /data/atlas/users/nterlind/venvs/thesis-ml
 
 # Run training (all settings come from configs)
 python -m thesis_ml.train "$@"
